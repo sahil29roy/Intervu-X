adc30a5efe84ce1c4b25cef8e1375e0c35d98465
sahil <sahil29roy@gmail.com>
2026-06-12 19:01:08 +0530

    feat: add environment variable configuration and Cloudinary file upload middleware

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -11,5 +11,8 @@ export const ENV = {
     STREAM_API_SECRET : process.env.STREAM_API_SECRET,
     CLIENT_URL : process.env.CLIENT_URL,
     JWT_SECRET : process.env.JWT_SECRET,
+    CLOUDINARY_CLOUD_NAME : process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY : process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET : process.env.CLOUDINARY_API_SECRET,
 }
 
